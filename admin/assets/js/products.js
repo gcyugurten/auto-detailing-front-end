21fcc1dbe128d55d9610cd2ff4dadfc663c3bab3
Jugurtha Gaci <jugurthagaci24@gmail.com>
2021-09-11 11:10:44 +0100

    're-upload'

---
+++ admin/assets/js/products.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('#products input#selectAll').change(function() {
+
+        $('#products input[type="checkbox"]').attr('checked', "checked")
+    })
+
+
+
+})
